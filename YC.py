fc16d49dc891bbb0a77455c6f4c867afda14a09f
zS20006781@estudiantes.uv.mx <zS20006781@estudiantes.uv.mx>
2023-03-17 14:37:15 -0600

    A

--- YC.py
+++ YC.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import codecs
-import plotly.express as px
+import plotly as px
 
 st.set_page_config(page_title="Canales de YouTube con más suscriptores",
                    page_icon="YL.png")
